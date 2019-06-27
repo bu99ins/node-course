3b02a8f051501044bf09bbac4fd53da71bfbc64d
Yury Krasavin <buggins@live.com>
2019-06-27 17:39:55 +0300

    append strings to output file.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "This file was created by Node.js");
+// fs.writeFileSync("notes.txt", "This file was created by Node.js");
+
+fs.appendFileSync("notes.txt", " I live in Yaroslavl.");
