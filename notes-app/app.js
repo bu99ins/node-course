fc02ae99fcf9bfac49ae4dfddd070916ec4ee9a3
Yury Krasavin <buggins@live.com>
2019-06-27 18:48:10 +0300

    Import the chalk npm module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,8 @@
-const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isEmail("hryak@uhus.ru"));
+const greenMsg = chalk.bold.green("Success!");
+console.log(greenMsg);
