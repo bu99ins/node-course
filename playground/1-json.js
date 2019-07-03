9ada0af0062c9b037dafb4770ba32959d68e02e4
Yury Krasavin <buggins@live.com>
2019-07-03 22:01:43 +0300

    read an object from a JSON file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
-const book = {
+/* const book = {
   title: "Ego is the Enemy",
   author: "Ryan Holiday"
 };
 
 const bookJson = JSON.stringify(book);
-fs.writeFileSync("1-json.json", bookJson);
+fs.writeFileSync("1-json.json", bookJson); */
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJson = dataBuffer.toString();
+const data = JSON.parse(dataJson);
+
+console.log(data.title);
