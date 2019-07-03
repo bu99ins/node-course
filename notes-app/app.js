3769936bc9e64624a32ab3122d486ecdde69907d
Yury Krasavin <buggins@live.com>
2019-07-03 18:32:51 +0300

    install yarn. make "add" and "remove" its commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,24 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version("1.1.0");
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+// Create add command
+yargs.command({
+  command: "add",
+  description: "Add a new note",
+  handler: function() {
+    console.log("Adding  a new note!");
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  description: "Remove a note",
+  handler: () => console.log("Removing the note!")
+});
+
+console.log(yargs.argv);
